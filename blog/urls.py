dde420670b2d1a4d0c897136fae7609b2149266d
ja-ho <ja-ho@localhost.localdomain>
2019-01-16 16:48:20 +0900

    add view.py in blog app

---
+++ blog/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import url
+from blog.views import *
+
+app_name = 'blog'
+url_patterns = [
+    #Example: /
+    url(r'^$', PostLV.as_view(), name='index'),
+
+    #Example: /post/ (same as /)
+    url(r'^post/$', PostLV.as_view(), name='post_list'),
+
+    #Example: /post/django-example
+    url(r'^post/(?P<slug>[-\w]+)/$', PostDV.as_view(), name='post_detail'),
+
+    #Example: /archive/
+    url(r'^archive/$', PostAV.as_view(), name='post_archive'),
+
+    #Example: /2019/
+    url(r'^(?P<year>\d{4})/$', PostYAV.as_view(), name='post_year_archive'),
+
+    #Example: /2019/fev/
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(),
+        name='post_month_archive'),
+
+    #Example: /2019/fev/16/
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$',
+        PostDAV.as_view(), name='post_day_archive'),
+
+    #Example: /today/
+    url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
+]
---
+++ bookmark/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from bookmark.views import BookmarkLV, BookmarkDV
+
+app_name = 'bookmark'
+url_patterns = [
+    #Class-based views
+    url(r'^$', BookmarkLV.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+]
